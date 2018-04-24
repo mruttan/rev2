a63e41a5bf1c78882b4b8b825deace49a5078072
Matthew Ruttan <matthew.ruttan5@gmail.com>
2018-04-24 16:10:01 -0400

    got the logout button rendering based on auth status working!

--- imports/ui/SiteHeader.js
+++ imports/ui/SiteHeader.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 import { Session } from 'meteor/session';
+import { withTracker } from 'meteor/react-meteor-data';
 
 export class SiteHeader extends React.Component {
   constructor(props) {
@@ -11,17 +14,34 @@ export class SiteHeader extends React.Component {
     Session.set('loggedIn', false);
   }
   // can't figure out how to render based on log in status, at least session works correctly
+  // update, this now works as intended! yay! i'm so smart
   render(props) {
-    return (
-      <div>
-        Site Header here
-        <button onClick={this.handleLogout.bind(this)}>Logout</button>
-      </div>
-    )
+    if (Session.get('loggedIn') === true) {
+      return (
+        <div>
+          Site Header here
+          <button onClick={this.handleLogout.bind(this)}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          Site Header here
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </div>
+      );
+    }
   }
 }
 
-export default SiteHeader;
+SiteHeader.propTypes = {
+  loggedIn: PropTypes.bool
+}
 
-
-//lifecycle method "onLogout()"
+export default withTracker(() => {
+  const loggedIn = Session.get('loggedIn');
+  return {
+    loggedIn
+  };
+})(SiteHeader);
